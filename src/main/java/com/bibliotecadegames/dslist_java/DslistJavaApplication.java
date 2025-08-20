92bfa5d34a2ef410932e2c20c11ae7a110a04669
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2025-08-20 18:00:49 -0300

    Criação do Maven project

---
+++ src/main/java/com/bibliotecadegames/dslist_java/DslistJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotecadegames.dslist_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bibliotecadegames/dslist_java/DslistJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotecadegames.dslist_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
